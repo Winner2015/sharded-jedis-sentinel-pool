0ada0040754d51d23bd87aec77cc0c9348d03d90
chenlongfei <chenlongfei@corp.netease.com>
2018-10-31 18:48:07 +0800

    test

--- src/test/java/redis/clients/jedis/MyShardedTest.java
+++ src/test/java/redis/clients/jedis/MyShardedTest.java
@@ -2,6 +2,9 @@ package redis.clients.jedis;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
 * @author chenlongfei
@@ -9,18 +12,31 @@ import java.util.List;
 public class MyShardedTest {
 	
 	public static void main(String[] args) {
-		List<MyShardedInfo> shards = new ArrayList<>();
+//		List<MyShardedInfo> shards = new ArrayList<>();
+//		
+//		MyShardedInfo info1 = new MyShardedInfo();
+//		MyShardedInfo info2 = new MyShardedInfo();
+//		
+//		shards.add(info1);
+//		shards.add(info2);
+//		
+//		MySharded pool = new MySharded(shards);
+//		
+//		pool.getShard("name1");
 		
-		MyShardedInfo info1 = new MyShardedInfo();
-		MyShardedInfo info2 = new MyShardedInfo();
 		
-		shards.add(info1);
-		shards.add(info2);
 		
-		MySharded pool = new MySharded(shards);
-		
-		pool.getShard("name1");
-		
+
+	Map<Boolean, List<Integer>> collectParti = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9)
+            .collect(Collectors.partitioningBy(it -> it % 2 == 0));
+	System.out.println("collectParti : " + collectParti);
+	
+	
+	Map<Integer, List<Integer>> collectParti2 = Stream.of(1, 2, 3, 4, 5, 6, 7, 8, 9)
+            .collect(Collectors.groupingBy(t -> t % 3));
+	System.out.println("collectParti2 : " + collectParti2);
+	
+	
 	}
 	
 	
