6eed7b1ff4b50c7b4f56eb0d5fa4c07c43791cd0
chenlongfei <chenlongfei@corp.netease.com>
2018-11-12 02:18:05 +0800

    add

--- src/main/java/test/Test2.java
+++ src/main/java/test/Test2.java
@@ -1,30 +1,64 @@
 package test;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Spliterator;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+import java.util.stream.AbstractPipeline;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.PipelineHelper;
+import java.util.stream.ReduceOps;
+import java.util.stream.Sink;
+import java.util.stream.Stream;
+import java.util.stream.StreamOpFlag;
+import java.util.stream.StreamShape;
+import java.util.stream.TerminalOp;
+import java.util.stream.ReduceOps.AccumulatingSink;
+import java.util.stream.ReduceOps.Box;
+import java.util.stream.ReduceOps.ReduceOp;
+import java.util.stream.ReduceOps.ReduceTask;
+import java.util.stream.ReferencePipeline.StatelessOp;
 
 public class Test2 {
 
 	public static void main(String[] args) {
 
-		Object rcvr = "a";
-		try {
-			MethodType mt = MethodType.methodType(int.class); // 方法签名
-			MethodHandles.Lookup l = MethodHandles.lookup(); // 调用者，也就是当前类。调用者决定有没有权限能访问到方法
-			MethodHandle mh = l.findVirtual(rcvr.getClass(), "hashCode", mt); // 分别是定义方法的类，方法名，签名
-
-			int ret;
-			try {
-				ret = (int) mh.invoke(rcvr); // 代码，第一个参数就是接收者
-				System.out.println(ret);
-			} catch (Throwable t) {
-				t.printStackTrace();
-			}
-		} catch (IllegalArgumentException | NoSuchMethodException | SecurityException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException x) {
-			x.printStackTrace();
-		}
+		List<String> list = Arrays.asList("China", "America", "Russia", "Britain");
+		List<String> result = list.stream()
+				.parallel()
+				.filter(e -> e.length() >= 4)
+				.map(e -> e.charAt(0))
+				.map(e -> String.valueOf(e))
+				.collect(Collectors.toList());
 	}
 }
+
+
+	public static <T> Stream<T> stream(Spliterator<T> spliterator, boolean parallel) {
+		Objects.requireNonNull(spliterator);
+
+		// 返回了一个由Head实现的Stream，三个参数分别代表流的数据源、特性组合、是否并行
+		return new ReferencePipeline.Head<>(spliterator, StreamOpFlag.fromCharacteristics(spliterator), parallel);
+	}
+
+AbstractPipeline(Spliterator<?> source,
+        int sourceFlags, boolean parallel) {
+this.previousStage = null;  //上一个stage指向null
+this.sourceSpliterator = source;
+this.sourceStage = this;  //源头stage指向自己
+this.sourceOrOpFlags = sourceFlags & StreamOpFlag.STREAM_MASK;
+// The following is an optimization of:
+// StreamOpFlag.combineOpFlags(sourceOrOpFlags, StreamOpFlag.INITIAL_OPS_VALUE);
+this.combinedFlags = (~(sourceOrOpFlags << 1)) & StreamOpFlag.INITIAL_OPS_VALUE;
+this.depth = 0;
+this.parallel = parallel;
+}
+
+
